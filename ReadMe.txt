d92dabce4ada9a33ac89a5fbecf2a0406ceae189
sahar <sahar.vahedi99@gmail.com>
2021-09-22 12:56:08 +0330

    updated

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,4 @@
 EventCalendar
 http://localhost/EventCalendar/www/sadaf/login.php
+
+C:\wamp64\bin\mariadb\mariadb10.4.10\bin>mysqldump -uroot -p eventcalendar > c:\wamp64\www\EventCalendar\EventCalendar_database.sql
---
+++ eventcalendar_menue_database.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO sadaf.SystemFacilities VALUES (8, 'رویداد', 2, 1, 'Event.php');
+INSERT INTO sadaf.SystemFacilities VALUES (10, 'مدیریت رویداد', 2, 1, 'ManageEvent.php');
+INSERT INTO sadaf.SystemFacilities VALUES (11, 'انواع رویداد', 2, 1, 'ManageEventTypes.php');
+INSERT INTO sadaf.SystemFacilities VALUES (12, 'فعالیت های رویداد', 2, 1, 'ManageEventTasks.php');
+
+INSERT INTO sadaf.UserFacilities (UserID, FacilityID) VALUES ('omid',8);
+INSERT INTO sadaf.UserFacilities (UserID, FacilityID) VALUES ('omid',10);
+INSERT INTO sadaf.UserFacilities (UserID, FacilityID) VALUES ('omid',11);
+INSERT INTO sadaf.UserFacilities (UserID, FacilityID) VALUES ('omid',12);
+
+
+INSERT INTO sadaf.FacilityPages (FacilityID, PageName) VALUES (8,'/Event.php');
+INSERT INTO sadaf.FacilityPages (FacilityID, PageName) VALUES (10,'/ManageEvent.php');
+INSERT INTO sadaf.FacilityPages (FacilityID, PageName) VALUES (11,'/ManageEventTypes.php');
+INSERT INTO sadaf.FacilityPages (FacilityID, PageName) VALUES (12,'/ManageEventTasks.php');
+
+
