31374a5ac118e92c81a920746c4171c2ddb4eeca
sahar <sahar.vahedi99@gmail.com>
2021-09-22 14:18:25 +0330

    update2

--- eventcalendar_menue_database.sql
+++ eventcalendar_menue_database.sql
@@ -3,6 +3,9 @@ INSERT INTO sadaf.SystemFacilities VALUES (8, 'رویداد', 2, 1, 'Event.php')
 INSERT INTO sadaf.SystemFacilities VALUES (10, 'مدیریت رویداد', 2, 1, 'ManageEvent.php');
 INSERT INTO sadaf.SystemFacilities VALUES (11, 'انواع رویداد', 2, 1, 'ManageEventTypes.php');
 INSERT INTO sadaf.SystemFacilities VALUES (12, 'فعالیت های رویداد', 2, 1, 'ManageEventTasks.php');
+INSERT INTO sadaf.SystemFacilities VALUES (13, 'فعالیت های رویداد', 2, 1, 'ManageEventTasks.php');
+
+
 
 INSERT INTO sadaf.UserFacilities (UserID, FacilityID) VALUES ('omid',8);
 INSERT INTO sadaf.UserFacilities (UserID, FacilityID) VALUES ('omid',10);
