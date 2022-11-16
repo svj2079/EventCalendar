f9fa90fc3006aae9130c01a8acad2b1abf387afb
omid milanifard <milanifard.o@gmail.com>
2022-11-16 19:22:14 +0330

    test

--- www/sadaf/calendar.php
+++ www/sadaf/calendar.php
@@ -2,7 +2,6 @@
     include "header.inc.php";
     include "classes\Event.class.php";
     include "classes/calendar.class.php";
-    //include "classes\EventTaskPerson.class.php";
     HTMLBegin();
     if(isset($_REQUEST["SelectedYear"]))
     {
